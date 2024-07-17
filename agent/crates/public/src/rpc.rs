49d60274a7f16dab85d9421fa0d245aa7afa9317
huanchao <huanchao@yunshan.net>
2024-07-17 21:05:06 +0800

    test: agent add chars

--- agent/crates/public/src/rpc.rs
+++ agent/crates/public/src/rpc.rs
@@ -118,12 +118,30 @@ pub mod remote_exec {
                 let Some(value) = p.value.as_ref() else {
                     return false;
                 };
-                for c in value.as_bytes() {
-                    match c {
-                        b'A'..=b'Z' | b'a'..=b'z' | b'0'..=b'9' | b'-' | b'_' => (),
-                        _ => return false,
-                    }
-                }
+                // for c in value.as_bytes() {
+                //     match c {
+                //         b'A'..=b'Z'
+                //         | b'a'..=b'z'
+                //         | b'0'..=b'9'
+                //         | b'-'
+                //         | b'_'
+                //         | b'|'
+                //         | b'"'
+                //         | b'/'
+                //         | b'.'
+                //         | b' '
+                //         | b'['
+                //         | b']'
+                //         | b'{'
+                //         | b'}'
+                //         | b'<'
+                //         | b'>'
+                //         | b'='
+                //         | b':'
+                //         | b'\'' => (),
+                //         _ => return false,
+                //     }
+                // }
             }
             true
         }
